713e9aee2d47c06e7975aa589d33a48d629579c6
SashaTkach31 <sashamixa14@gmail.com>
2024-12-01 21:53:21 +0200

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,18 @@ closePanel.addEventListener('click', function () {
     document.body.classList.remove('panel-open');
     menuToggle.classList.remove('open');
 });
+
+$('.owl-carousel').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 6500,
+    autoplaySpeed: 1000,
+    loop: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        1100: {
+            items: 1
+        }
+    }
+})
